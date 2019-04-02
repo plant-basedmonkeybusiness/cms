6d53f85eb8fcd2f11993f2e709b92b97cfd269e7
HakonArnarS <43438456+HakonArnarS@users.noreply.github.com>
2019-04-02 12:10:55 +0000

    search virkar

--- main.js
+++ main.js
@@ -1,13 +1,36 @@
 const db = firebase.firestore();
+const mainDiv = document.querySelector('.container');
 
-const button = document.querySelector('button');
-button.onclick = ()=> {
-    let nameValue = document.getElementById('name').value;
+
+const searchBtn = document.querySelector('#searchBtn');
+console.log(searchBtn); 
+searchBtn.addEventListener("click", ()=> { 
+    const keyword = document.querySelector("#search").value;
+    fetch("http://localhost:3000/plants?plant=" +keyword)
+    .then(r => r.json())
+    .then(data=>{
+        mainDiv.innerHTML = "";
+        data.forEach( data =>{
+             
+            mainDiv.innerHTML +=
+            `
+            <div class="card">
+                <h2>${data.scientific_name}</h2>
+            </div>
+            `
+        })
+    })   
+})
+
+
+// const button = document.querySelector('button');
+// button.onclick = ()=> {
+//     let nameValue = document.getElementById('name').value;
     
-    console.log(nameValue);
+//     console.log(nameValue);
     
-    db.collection("plants").doc(nameValue).set({
-        name: nameValue
-    })
-} 
-console.log(db.collection("plants"))
+//     db.collection("plants").doc(nameValue).set({
+//         name: nameValue
+//     })
+// } 
+
